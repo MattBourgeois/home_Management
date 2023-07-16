9143b0318904515285e0f84760491e3c006cea43
MattBourgeois <Bourgeoismatthew6@gmail.com>
2023-07-16 16:30:35 -0400

    trying to get full calender to show

--- Flask_app/controller/routes.py
+++ Flask_app/controller/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, redirect, request, session
+from flask import Flask, render_template, redirect, request, session
 from Flask_app import app
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	booked_dates = ['2023-07-18', '2023-07-19', '2023-07-25', '2023-07-26']
+	return render_template("index.html", booked_dates=booked_dates)
