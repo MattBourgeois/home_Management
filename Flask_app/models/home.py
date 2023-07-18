226179c233c8c50cd07b44c44b2110a150c3abf2
MattBourgeois <Bourgeoismatthew6@gmail.com>
2023-07-17 21:48:15 -0400

    added a whole class

---
+++ Flask_app/models/home.py
@@ -0,0 +1,17 @@
+from Flask_app.config.mysqlconnection import connectToMySQL
+
+class Home:
+	db_name = 'Home_management'
+	self.id = data["id"]
+	self.Name = data["Name"]
+	self.Address = data["Address"]
+	self.DaysRented = data["DaysRented"]
+	self.ServiceRequest = data["ServiceRequest"]
+	self.Created_at	= data["Created_at"]
+	self.Updated_at = data["Updated_at"]
+	self.user = None
+
+	@classmethod
+	def save_request(cls, data):
+		query = "INSTER INTO Home_management (Name, Address, ServiceRequest) VALUES (%(Name)s, %(Name)s, %(Address)s, %(ServiceRequest)s);"
+		return connectToMySQL(cls, db_name).query_db(query, data)
