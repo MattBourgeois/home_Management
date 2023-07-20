f99c84bdf0e7ef350c59d3de096a9287435421b8
MattBourgeois <Bourgeoismatthew6@gmail.com>
2023-07-19 20:45:06 -0400

    route for submit request

--- Flask_app/models/home.py
+++ Flask_app/models/home.py
@@ -2,7 +2,7 @@ from Flask_app.config.mysqlconnection import connectToMySQL
 
 class Home:
 	db_name = 'Home_management'
-	self.id = data["id"]
+	# self.id = data["id"]
 	self.Name = data["Name"]
 	self.Address = data["Address"]
 	self.DaysRented = data["DaysRented"]
@@ -13,5 +13,5 @@ class Home:
 
 	@classmethod
 	def save_request(cls, data):
-		query = "INSTER INTO Home_management (Name, Address, ServiceRequest) VALUES (%(Name)s, %(Address)s, %(Address)s, %(ServiceRequest)s);"
+		query = "INSTER INTO Home_management (Name, Address, ServiceRequest) VALUES (%(Name)s, %(Address)s, %(ServiceRequest)s);"
 		return connectToMySQL(cls, db_name).query_db(query, data)
