d010cb851db6ff63257ad0ff4e6a18473b265754
MattBourgeois <Bourgeoismatthew6@gmail.com>
2023-07-22 21:38:01 -0400

    flash work

--- Flask_app/models/home.py
+++ Flask_app/models/home.py
@@ -1,17 +1,18 @@
 from Flask_app.config.mysqlconnection import connectToMySQL
 
-class Home:
+class House:
 	db_name = 'Home_management'
-	# self.id = data["id"]
-	self.Name = data["Name"]
-	self.Address = data["Address"]
-	self.DaysRented = data["DaysRented"]
-	self.ServiceRequest = data["ServiceRequest"]
-	self.Created_at	= data["Created_at"]
-	self.Updated_at = data["Updated_at"]
-	self.user = None
+	def __init__(self, data):
+		self.id = data["id"]
+		self.Name = data["Name"]
+		self.Address = data["Address"]
+		self.DaysRented = data["DaysRented"]
+		self.ServiceRequest = data["ServiceRequest"]
+		self.Created_at	= data["Created_at"]
+		self.Updated_at = data["Updated_at"]
+		self.user = None
 
-	@classmethod
-	def save_request(cls, data):
-		query = "INSTER INTO Home_management (Name, Address, ServiceRequest) VALUES (%(Name)s, %(Address)s, %(ServiceRequest)s);"
-		return connectToMySQL(cls, db_name).query_db(query, data)
+		@classmethod
+		def save_request(cls, data):
+			query = "INSTER INTO Home_management (Name, Address, ServiceRequest) VALUES (%(Name)s, %(Address)s, %(ServiceRequest)s);"
+			return connectToMySQL(cls, db_name).query_db(query, data)
